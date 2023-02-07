6a7911113bb831ffce44bc967810f4d2dccfac99
Vivekanand Vernekar <116813193+vivekanand-vr@users.noreply.github.com>
2023-02-07 12:24:34 +0530

    Update First.cpp

--- First.cpp
+++ First.cpp
@@ -19,24 +19,6 @@ void print(int x)
 void VV07()
 {
     
-   int N, P;
-    cin >> N >> P;
-
-    vector<int> dp(N + 1, 0), dp2(N + 1, 0);
-    dp[1] = dp2[1] = mod_inverse(2);
-
-    int inv2 = mod_inverse(2);
-    for (int i = 2; i <= N; i++){
-        int y = binpow(P, i - 1);
-        y = mod_inverse(y);
-        int val = dp[i - 1] + mul(binpow(y, 2), inv2) + mul(y, dp2[i - 1]);
-        val %= mod1;
-        dp[i] = val;
-        dp2[i] = (dp2[i - 1] + mul(y, inv2)) % mod1;
-    }
-    for (int i = 1; i <= N; i++)
-        cout << dp[i] << ' ';
-    cout << endl;
 
 }
 
